00ea6934c394f8f1c9106de55337bd87df124371
Matheus Cavalcanti <mthcavalcanti@gmail.com>
2018-12-30 01:53:04 -0300

    melhorias

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,6 @@ import beans.Musica;
 import beans.Pessoa;
 import algoritmo.Knn;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
@@ -131,61 +128,47 @@ public class Main {
 		    } catch (IOException ex) {
 		       System.out.println("Unable to open music file." + ex.toString());
 		    }
-		
-		
+			
 		Pessoa pessoa5 = new Pessoa("gustavo.c", "12345", musicas5);
-		/*
-		Musica duskTillDawn = new Musica(0,0.15529,1,
-				0.537062,0,0.038744,0.145005,0.000006,0.15996,0.011935,0.841239,0.414108);
 		
 		Knn knn = new Knn();
 		
-		for(int i = 0; i < pessoa5.getMusicas().size(); i++) {
-			System.out.println(knn.obterDistanciaEuclidiana(duskTillDawn, pessoa5.getMusicas().get(i)));
-			
-		}
-		*/
-		
-		  File file = new File("C:/Users/Agostinho (Carrara)/Desktop/Pastas e arquivos/Relatório + vetor de caracteristica/data.arff");
-
-	        FileReader fr = null;
-	        BufferedReader br = null;
-
-	        List<Musica> base = new ArrayList<>();
-
-	        try {
-	            AbrirArquivo.populateInstances(AbrirArquivo.openFile(file, fr, br), base);
-
-	        } catch (IOException e) {
-	            e.printStackTrace();
-
-	        } finally {
-	            AbrirArquivo.close(br, fr);
-
-	        }
-	               
-	        
-	        Knn knn = new Knn();
-	        int count = 1;
-	        
-	        for(int i = 0; i < base.size(); i++) {
-	        	for(int j = 0; j < pessoa2.getMusicas().size(); j++) {
-	        		double dist = knn.obterDistanciaEuclidiana(base.get(i), pessoa2.getMusicas().get(j));
-	        		if(dist <= 0.53) {
-	        			musicasRecomendadas.add(base.get(i));
-	        		}
-	        		
-	        		count++;
-	        	}
-	        		
-	        }
-	        
-	        
-	        for(int x = 0; x < musicasRecomendadas.size(); x++) {
-	        	System.out.println(musicasRecomendadas.get(x).getDanceability());
-	        }
-		
-		
-	}
+		//Abrir base de dados do arquivo arff
+		List<Musica> base = new ArrayList<>();
+        
+        try {
+		       Stream<String> lines = Files.lines(Paths.get("C:/Users/Agostinho (Carrara)/Desktop/Pastas e arquivos/Relatório + vetor de caracteristica/data.arff"));
+		       lines.forEach((String t) -> {
+		          String[] parse = t.split(",");
+		          if(parse.length<12) return;
+		          base.add(new Musica(Double.parseDouble(parse[0]), 
+		        		  Double.parseDouble(parse[1]), Double.parseDouble(parse[2]),
+		        		  Double.parseDouble(parse[3]), Double.parseDouble(parse[4]),
+		        		  Double.parseDouble(parse[5]), Double.parseDouble(parse[6]),
+		        		  Double.parseDouble(parse[7]), Double.parseDouble(parse[8]),
+		        		  Double.parseDouble(parse[9]), Double.parseDouble(parse[10]),
+		        		  Double.parseDouble(parse[11])));
+		       });
+		    } catch (IOException ex) {
+		       System.out.println("Unable to open music file." + ex.toString());
+		    }
+               
+        int count = 1;
+        
+        
+        for(int i = 20; i < base.size(); i++) {
+        	for(int j = 0; j < pessoa1.getMusicas().size(); j++) {
+        		
+    			double dist = knn.obterDistanciaEuclidiana(base.get(i), pessoa1.getMusicas().get(j));
+    			
+    			if(dist < 0.53) {
+    				System.out.println( count + " " + dist);
+    			}
+    			count++;
+        		
+        		}
+        	}
+        }        
+        
+	  }
 	
-}
